a1ae313b78873fdc446a335b750c81a21b86c852
manish goyal <manishgoyal0703@gmail.com>
2023-09-06 11:02:26 +0530

    Builder design pattern implemented

---
+++ src/main/java/com/solidprinciples/designPatterns/creational/builder/UserDTO.java
@@ -0,0 +1,11 @@
+package com.solidprinciples.designPatterns.creational.builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
